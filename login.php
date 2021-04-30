4e72a4918a63421af24677c3a71665275cebc654
Karan Sah <iit2019046@iiita.ac.in>
2021-04-30 12:19:02 +0530

    structure updated

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
 $host="localhost";
 $user="root";
 $password="";
-$db="login";
+$db="emandi";
 
 $mysqli =  mysqli_connect($host,$user,$password,$db);
 # mysqli_select_db($db);
@@ -15,15 +15,24 @@ if(isset($_POST['username'])){
     $uname=$_POST['username'];
     $password=$_POST['password'];
     
-    $sql="SELECT * FROM `login_table` where username='".$uname."'AND password='".$password."' limit 1";
+    $sql1="SELECT * FROM `civilian` where username='".$uname."'AND password='".$password."' limit 1";
+    $sql2="SELECT * FROM `farmer` where username='".$uname."'AND password='".$password."' limit 1";
+    $sql3="SELECT * FROM `retailler` where username='".$uname."'AND password='".$password."' limit 1";
+    $sql4="SELECT * FROM `wholeseller` where username='".$uname."'AND password='".$password."' limit 1";
     
-    $result=mysqli_query($mysqli,$sql);
+    $result1=mysqli_query($mysqli,$sql1);
+    $result2=mysqli_query($mysqli,$sql2);
+    $result3=mysqli_query($mysqli,$sql3);
+    $result4=mysqli_query($mysqli,$sql4);
     #$row = mysqli_fetch_array($result);
-    $row = mysqli_num_rows($result);
+    $row1 = mysqli_num_rows($result1);
+    $row2 = mysqli_num_rows($result2);
+    $row3 = mysqli_num_rows($result3);
+    $row4 = mysqli_num_rows($result4);
     #$total = $row[0];
     #echo $result;
 
-    if($row == 1){
+    if($row1 == 1 || $row2 == 1 ||$row3 == 1 ||$row4 == 1  ){
         //echo '<script>alert("succesfully logged in")</script>';
         echo "<script> window.location.assign('role.html'); </script>";
         //header('Location: login.html');
---
+++ wholeseller/pay.php
@@ -0,0 +1,5 @@
+<form action="pay.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
