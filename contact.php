8557a4a091f00f5105b252361f574aa69daa5a00
Karan Sah <iit2019046@iiita.ac.in>
2021-05-01 09:55:04 +0530

    contact option added

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+
+//echo "working";
+
+$rname = $_POST['name'];
+$ral = $_POST['aadhar_no'];
+$rphone = $_POST['phone'];
+$rmes = $_POST['message'];
+
+$host="localhost";
+$user="root";
+$password="";
+$db="emandi";
+
+$mysqli =  mysqli_connect($host,$user,$password,$db);
+$sql="INSERT INTO `message` values ('".$rname."','".$ral."','".$rphone."','".$rmes."')";
+mysqli_query($mysqli,$sql);
+echo "<script> window.location.assign('index.html'); </script>";
+mysqli_close($mysqli);
+exit();
+
+
+
+?>
