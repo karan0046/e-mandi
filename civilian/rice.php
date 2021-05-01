7a0504104b0dfdbe4754948477033b47cfc73096
Karan Sah <iit2019046@iiita.ac.in>
2021-05-01 21:53:17 +0530

    transaction and add self added

---
+++ civilian/rice.php
@@ -0,0 +1,66 @@
+<?php
+
+//echo "working";
+
+$ad = $_POST['Addhaar'];
+$name = $_POST['name'];
+$category = $_POST['category'];
+$item = $_POST['bank1'];
+$ph = $_POST['Mobile'];
+$addr = $_POST['Address'];
+$price = $_POST['Price'];
+$quan = $_POST['Quantity'];
+/*
+<option value="dem">Select your crop name</option>
+            <option value="sbi">Rice</option>
+            <option value="hdfc">Wheat</option>
+            <option value="BoB">Maize</option>
+            <option value="PNB">Lentils</option>
+            <option value="icici">Onion</option>
+            <option value="other">Potato</option>
+            <option value="dem">Turmeric</option>
+            <option value="sbi">Apple</option>
+            <option value="hdfc">Grapes</option>
+            <option value="BoB">Bringal</option>
+            <option value="PNB">Orange</option>
+            <option value="icici">Cauliflower</option>
+            <option value="other">Cashew</option>
+*/
+if($item == "sbi"){
+    $item = "Rice";
+}else if($item == "hdfc"){
+    $item = "Wheat";
+}else if($item == "Bob"){
+    $item = "Maize";
+}else if($item == "PNB"){
+    $item = "Lentils";
+}else if($item == "icici"){
+    $item = "Onion";
+}else if($item  == "other"){
+    $item = "Potato";
+}
+
+
+$host="localhost";
+$user="root";
+$password="";
+$db="emandi";
+
+$mysqli =  mysqli_connect($host,$user,$password,$db);
+
+$sql="SELECT * FROM `transaction`;";
+$result=mysqli_query($mysqli,$sql);
+$id = mysqli_num_rows($result) + 1;
+//echo $id;
+mysqli_free_result($result);
+
+
+$insert="INSERT INTO `add_request` values ('".$ad."','".$name."','".$category."','".$item."','".$ph."','".$addr."','".$price."','".$quan."')";
+mysqli_query($mysqli,$insert);
+echo '<script>alert("Wait untill Admin reviews your request")</script>';
+echo "<script> window.location.assign('apple.html'); </script>";
+mysqli_close($mysqli);
+exit();
+
+
+?>
--- wholeseller/pay.php
+++ wholeseller/pay.php
@@ -1,5 +1,40 @@
-<form action="pay.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
+<?php
+
+//echo "working";
+
+$cnum = $_POST['lastname'];
+$cmonth = $_POST['month'];
+$ccvv = $_POST['cvv'];
+$chname = $_POST['firstname'];
+$camount = $_POST['rs'];
+$bname = $_POST['bank'];
+$cardt = $_POST['card'];
+$date = date('d F, Y (l)');
+$time = date('h:i:s A');
+$status = 0;
+
+//echo $date , $time;
+
+$host="localhost";
+$user="root";
+$password="";
+$db="emandi";
+
+$mysqli =  mysqli_connect($host,$user,$password,$db);
+
+$sql="SELECT * FROM `transaction`;";
+$result=mysqli_query($mysqli,$sql);
+$id = mysqli_num_rows($result) + 1;
+//echo $id;
+mysqli_free_result($result);
+
+
+$insert="INSERT INTO `transaction` values ('".$id."','".$date."','".$time."','".$status."','".$cardt."','".$bname."','".$cnum."','".$cmonth."','".$ccvv."','".$chname."','".$camount."')";
+mysqli_query($mysqli,$insert);
+echo '<script>alert("Transaction Under Review ! Redirecting to homepage")</script>';
+echo "<script> window.location.assign('start.html'); </script>";
+mysqli_close($mysqli);
+exit();
+
+
+?>
